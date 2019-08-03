f3898ed50cc0db6d4211f4a3308ea3c3d52c41c7
LiamH <liamhearts@gmail.com>
2019-08-03 13:52:58 -0700

    navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,37 +1,37 @@
 import React from 'react';
 import './Navbar.css';
-
+import logo from "../images/TestLogo3.png";
 export default function Navbar() {
     return (
-        <nav className="navbar navbar-expand-lg navbar-light text-white">
-                <a className="navbar-brand" href="#">Navbar</a>
+        <div>
+        <nav className="navbar navbar-expand-lg">
+                <a className="navbar-brand" href="#"><img id="navLogo" src={logo}></img></a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNavDropdown">
                     <ul className="navbar-nav">
-                        <li className="nav-item active">
-                            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Features</a>
-                        </li>
-                        <li className="nav-item">
-                            <a className="nav-link" href="#">Pricing</a>
-                        </li>
-                        <li className="nav-item dropdown">
+                    <li className="nav-item dropdown">
                             <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                Dropdown link
+                                Browse
                             </a>
                             <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                <a className="dropdown-item" href="#">Action</a>
-                                <a className="dropdown-item" href="#">Another action</a>
-                                <a className="dropdown-item" href="#">Something else here</a>
+                                <a className="dropdown-item" href="#">Illustration</a>
+                                <a className="dropdown-item" href="#">Graphic Design</a>
+                                <a className="dropdown-item" href="#">Photography</a>
                             </div>
                         </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Discover</a>
+                        </li>
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">About</a>
+                        </li>
+                       
                     </ul>
                 </div>
             </nav>
+            </div>
     )
 }
 
