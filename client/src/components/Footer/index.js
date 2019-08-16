11e805fae6f80c56796b2b826fa373fbcfb6d85e
Jim Tatarakis <jimtatarakis@gmail.com>
2019-08-15 21:31:21 -0700

    footer and nav background

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+export default function Footer() {
+    return (
+        <React.Fragment>
+            <div className='footer-grid'>
+            
+            </div>
+        </React.Fragment>
+    )
+}
