186dad7c7ba747a23b2e72e0610a2c8d6c072e7c
Alex Engelmann <alex.engelmann@gmail.com>
2019-08-17 12:33:53 -0700

    basic jumbotron up

---
+++ client/src/pages/home/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Jumbotron from '../../components/Jumbotron/Jumbotron';
+
+export default function HomePage() {
+    return (
+        <div>
+            <Jumbotron></Jumbotron>
+        </div>
+    )
+}
