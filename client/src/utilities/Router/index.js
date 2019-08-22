80bba8e098a97568fa7020d9e7835587a04a3995
Ghost Ghost <ghost77@Ghosts-MacBook-Pro.local>
2019-08-21 19:23:35 -0700

    added goggle form

--- client/src/utilities/Router/index.js
+++ client/src/utilities/Router/index.js
@@ -6,7 +6,7 @@ import About from '../../pages/about'
 import Artists from '../../pages/artists'
 // import * as Artist from '../../pages/artist'
 import Artwork from '../../pages/artwork'
-// import * as Contact from '../../pages/contact'
+import Contact from '../../pages/contact'
 import ShoppingCart from '../../pages/shoppingCart'
 
 function AppRouter() {
@@ -21,8 +21,8 @@ function AppRouter() {
                 <Route path="/about" component={About} />
                 <Route path="/artists" component={Artists} />
                 <Route path="/artwork" component={Artwork} />
-                {/* <Route path="/contact" component={ContactPage} />
-                <Route path="/artist/:id" component={ArtistPage} /> */}
+                 <Route path="/contact" component={Contact} />
+                {/*<Route path="/artist/:id" component={ArtistPage} /> */}
                 <Route path="/shopcart" component={ShoppingCart} />
             </Switch>
          
